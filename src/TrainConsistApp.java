27bcf9add4f2b3791db7a38713a3f80b99b4cdd0
Nik-DevLAB <na8647@srmist.edu.in>
2026-04-06 14:24:46 +0530

    feature testcase 17

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -5,20 +5,10 @@ import java.util.*;
 public class TrainConsistApp {
     public static void main(String[] args) {
 
-        int[] arr = {72, 56, 24, 70, 60};
+        String[] bogies = {"Sleeper","AC Chair","First Class","General","Luxury"};
 
-        int n = arr.length;
+        Arrays.sort(bogies);
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(bogies));
     }
 }
