314c542e9aeb5ce7886c5e1311c478b3b94fff0b
Nik-DevLAB <na8647@srmist.edu.in>
2026-04-06 14:27:17 +0530

    feature testcase 19

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -6,12 +6,20 @@ public class TrainConsistApp {
         String[] ids = {"BG101","BG205","BG309","BG412","BG550"};
         String key = "BG309";
 
+        int low = 0, high = ids.length - 1;
         boolean found = false;
 
-        for (String id : ids) {
-            if (id.equals(key)) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int cmp = key.compareTo(ids[mid]);
+
+            if (cmp == 0) {
                 found = true;
                 break;
+            } else if (cmp > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
