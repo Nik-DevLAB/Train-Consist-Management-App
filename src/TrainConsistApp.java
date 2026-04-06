1fcdf769d56ce3d2cf9f35806de18cb6b0c12543
Nik-DevLAB <na8647@srmist.edu.in>
2026-04-06 14:24:02 +0530

    feature testcase 16

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,42 +1,24 @@
 package src;
 
-class CargoSafetyException extends RuntimeException {
-    public CargoSafetyException(String msg) {
-        super(msg);
-    }
-}
-
-class GoodsBogie {
-    String type;
-    String cargo;
-
-    GoodsBogie(String type) {
-        this.type = type;
-    }
-
-    void assignCargo(String cargo) {
-        try {
-            if (type.equals("Rectangular") && cargo.equals("Petroleum"))
-                throw new CargoSafetyException("Unsafe cargo assignment");
-            this.cargo = cargo;
-            System.out.println("Cargo assigned: " + cargo);
-        } catch (CargoSafetyException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("Assignment attempt completed");
-        }
-    }
-}
+import java.util.*;
 
 public class TrainConsistApp {
     public static void main(String[] args) {
 
-        GoodsBogie b1 = new GoodsBogie("Cylindrical");
-        GoodsBogie b2 = new GoodsBogie("Rectangular");
+        int[] arr = {72, 56, 24, 70, 60};
 
-        b1.assignCargo("Petroleum");   // safe
-        b2.assignCargo("Petroleum");   // unsafe
+        int n = arr.length;
 
-        System.out.println("Program continues...");
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
