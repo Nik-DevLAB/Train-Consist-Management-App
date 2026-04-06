818bff77655866e3466f58386e99a0497a61ee74
Nik-DevLAB <na8647@srmist.edu.in>
2026-04-06 14:18:51 +0530

    feature testcase 13

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,26 +1,38 @@
 package src;
 
 import java.util.*;
+import java.util.stream.*;
 
 class Bogie {
-    String type, cargo;
-    Bogie(String type, String cargo) {
-        this.type = type;
-        this.cargo = cargo;
+    int capacity;
+    Bogie(int capacity) {
+        this.capacity = capacity;
     }
 }
 
 public class TrainConsistApp {
     public static void main(String[] args) {
-        List<Bogie> list = Arrays.asList(
-                new Bogie("Cylindrical", "Petroleum"),
-                new Bogie("Open", "Coal"),
-                new Bogie("Box", "Grain")
-        );
 
-        boolean safe = list.stream()
-                .allMatch(b -> !b.type.equals("Cylindrical") || b.cargo.equals("Petroleum"));
+        List<Bogie> list = new ArrayList<>();
+        for (int i = 0; i < 100000; i++)
+            list.add(new Bogie((int)(Math.random() * 100)));
 
-        System.out.println(safe ? "Train is Safe" : "Train is Unsafe");
+        // Loop
+        long start1 = System.nanoTime();
+        List<Bogie> res1 = new ArrayList<>();
+        for (Bogie b : list)
+            if (b.capacity > 60)
+                res1.add(b);
+        long end1 = System.nanoTime();
+
+        // Stream
+        long start2 = System.nanoTime();
+        List<Bogie> res2 = list.stream()
+                .filter(b -> b.capacity > 60)
+                .collect(Collectors.toList());
+        long end2 = System.nanoTime();
+
+        System.out.println("Loop Time: " + (end1 - start1));
+        System.out.println("Stream Time: " + (end2 - start2));
     }
 }
